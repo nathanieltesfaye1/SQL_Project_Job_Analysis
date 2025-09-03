98bf12a6f64fee98c808c1a72b11aef13f53a163
Nathaniel Tesfaye <nathaniel.tesfaye@kcl.ac.uk>
2025-09-03 14:06:20 +0100

    Query 5 - Optimal Skills

---
+++ project_sql/5_optimal_skills.sql
@@ -0,0 +1,27 @@
+/*
+What are the most 'optimal' (i.e. in high demand AND high paying) skills to learn/develop?
+*/
+
+SELECT 
+    skills_dim.skill_id,
+    skills_dim.skills,
+    COUNT(skills_job_dim.job_id) AS "demand_count",
+    ROUND(AVG(job_postings_fact.salary_year_avg), 2) AS avg_salary
+FROM
+    job_postings_fact
+    INNER JOIN
+    skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN
+    skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Software Engineer'
+    AND salary_year_avg IS NOT NULL
+GROUP BY
+    skills_dim.skill_id
+HAVING
+    COUNT(skills_job_dim.job_id) > 10
+ORDER BY
+    avg_salary DESC,
+    demand_count DESC
+LIMIT
+    25
